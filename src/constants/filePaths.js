44fd1d3aabc81aaaba67f2f520e6f508e98f6561
Mina Lee <mal0070@naver.com>
2024-11-09 19:28:15 +0900

    refactor: validateItem()을 ShoppingController에서 util의 폴더의 파일로 이동(ItemValidator.js)

--- src/constants/filePaths.js
+++ src/constants/filePaths.js
@@ -1,13 +1,8 @@
 import path from 'path';
-/*import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);*/
 
 const FILE_PATHS = {
     PRODUCTS: path.resolve(process.cwd(),'public','products.md'),
     PROMOTION: path.resolve(process.cwd(),'public', 'promotions.md')
 };
-
 
 export default FILE_PATHS;
--- src/controllers/ShoppingController.js
+++ src/controllers/ShoppingController.js
@@ -4,6 +4,7 @@ import fetchData from '../utils/DataGenerator.js';
 import FILE_PATHS from '../constants/filePaths.js';
 import Stock from '../models/Stock.js';
 import { Console } from '@woowacourse/mission-utils';
+import validateItem from '../utils/ItemValidator.js';
 
 class ShoppingController {
   async run() {
@@ -14,41 +15,16 @@ class ShoppingController {
     let items;
     let isValidateItem = false;
 
-    while (!isValidateItem) {
+    while(!isValidateItem){
       try {
         items = await InputView.readItem();
-        this.validateItem(items, stock);
+        validateItem(items, stock);
         isValidateItem = true;
       } catch (error) {
         Console.print(error.message);
       }
     }
-    //테스트 통과하게끔 파일 불러오는 로직 수정
-
     //const promotions = await fetchData(FILE_PATHS.PROMOTION);
-  }
-
-  validateItem(items, stock) {
-    for (const item of items) {
-      const targetProducts = stock.getProductsByName(item.name);
-      if (!targetProducts || targetProducts.length === 0) {
-        throw new Error(
-          '[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.'
-        );
-      }
-
-      let totalQuantities = 0;
-      targetProducts.forEach(
-        (product) => (totalQuantities += product.quantity)
-      );
-      if (totalQuantities < item.quantity) {
-        throw new Error(
-          '[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.'
-        );
-      }
-    }
-    Console.print('구매시작');
-    //return true;
   }
 }
 
